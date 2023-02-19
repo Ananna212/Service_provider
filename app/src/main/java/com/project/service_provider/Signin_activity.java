d2c331b1a4c0503e79cbc8b49d5b399a24360727
saif-uddin-shrabon <saif06065@gmail.com>
2023-02-19 21:23:59 +0600

    spinner add

--- app/src/main/java/com/project/service_provider/Signin_activity.java
+++ app/src/main/java/com/project/service_provider/Signin_activity.java
@@ -2,28 +2,54 @@ package com.project.service_provider;
 
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.project.service_provider.admin.admin_Doctor;
+import com.project.service_provider.admin.admin_transport;
+import com.project.service_provider.admin.transportHelper;
 
-public class Signin_activity extends AppCompatActivity  {
+import java.io.InputStream;
+import java.util.Random;
 
+public class Signin_activity extends AppCompatActivity {
+
+    private AlertDialog progressAlertDialog;
     EditText username, email, address, pass, confirmpass;
     Button Register, logedIn;
+    ImageView imgupl;
+    Uri imageUri;
+    Bitmap bitmap;
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firebaseFirestore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +66,30 @@ public class Signin_activity extends AppCompatActivity  {
         Register = findViewById(R.id.Register);
         logedIn = findViewById(R.id.logedIn);
 
+        imgupl = findViewById(R.id.imguploadp);
+
+        //drop down item//-------------------------------------------
+        String[] type1 = new String[]{"Medical ","Trasnport ","Education "};
+        //getting item from drop down---------------------------
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(
+                Signin_activity.this,
+                R.layout.drop_down_item,
+                type1
+        );
+        AutoCompleteTextView autoCompleteTextView1= findViewById(R.id.drop1);
+        autoCompleteTextView1.setAdapter(adapter1);
+
+        imgupl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Image File"), 1);
+
+            }
+        });
+
 
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +101,7 @@ public class Signin_activity extends AppCompatActivity  {
                 String Pass = pass.getText().toString();
                 String Confirmpass = confirmpass.getText().toString();
                 System.out.println("Output Email " + Email);
+                String category = autoCompleteTextView1.getText().toString();
 
                 if (TextUtils.isEmpty(Username)) {
                     username.setError("Username can't empty");
@@ -74,27 +125,69 @@ public class Signin_activity extends AppCompatActivity  {
 //                    pass.requestFocus();
 //                }
                 else {
+                    FirebaseStorage storage = FirebaseStorage.getInstance();
+                    StorageReference uploder = storage.getReference("Image1"+new Random().nextInt(50));
 
-                    firebaseAuth.createUserWithEmailAndPassword(Email, Pass)
-                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    uploder.putFile(imageUri)
+                            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
                                 @Override
-                                public void onSuccess(AuthResult authResult) {
-                                    firebaseFirestore.collection("users")
-                                            .document(FirebaseAuth.getInstance().getUid())
-                                            .set(new model(Username, Email, Address, Pass, Confirmpass));
-                                    Toast.makeText(Signin_activity.this, "Registration successfull!", Toast.LENGTH_LONG).show();
-                                    Intent intent = new Intent(Signin_activity.this,Login_activity.class);
-                                    startActivity(intent);
-                                    finish();
+                                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                    if (progressAlertDialog == null) {
+                                        AlertDialog.Builder builder2 = new AlertDialog.Builder(Signin_activity.this);
+                                        builder2.setTitle("Alert !");
+                                        builder2.setMessage("Uploaded: 0%");
+                                        builder2.setPositiveButton(
+                                                "Ok",
+                                                new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int id) {
+                                                        dialog.cancel();
+                                                    }
+                                                });
 
+                                        progressAlertDialog = builder2.create();
+                                        progressAlertDialog.show();
+                                    }
+
+                                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                                    progressAlertDialog.setMessage("Uploaded: " + (int) progress + "%");
                                 }
-                            }).addOnFailureListener(new OnFailureListener() {
+                            })
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                                 @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(Signin_activity.this, "Registration failed!", Toast.LENGTH_LONG).show();
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                    uploder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+
+                                            firebaseAuth.createUserWithEmailAndPassword(Email, Pass)
+                                                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                                        @Override
+                                                        public void onSuccess(AuthResult authResult) {
+                                                            firebaseFirestore.collection("users")
+                                                                    .document(FirebaseAuth.getInstance().getUid())
+                                                                    .set(new model(Username, Email, Address, Pass, Confirmpass,uri.toString()));
+                                                            Toast.makeText(Signin_activity.this, "Registration successfull!", Toast.LENGTH_LONG).show();
+                                                            Intent intent = new Intent(Signin_activity.this, Login_activity.class);
+                                                            startActivity(intent);
+                                                            finish();
+
+                                                        }
+                                                    }).addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            Toast.makeText(Signin_activity.this, "Registration failed!", Toast.LENGTH_LONG).show();
+
+                                                        }
+                                                    });
+                                        }
+                                    });
 
                                 }
                             });
+
+                    //old code
+
                 }
             }
         });
@@ -108,5 +201,24 @@ public class Signin_activity extends AppCompatActivity  {
             }
         });
     }
-}
 
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            imageUri = data.getData();
+
+            try {
+                InputStream inputStream = Signin_activity.this.getContentResolver().openInputStream(imageUri);
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                imgupl.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+
+            }
+        }
+
+    }
+}
