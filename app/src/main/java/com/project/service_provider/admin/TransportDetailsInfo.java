7272cf81be5120b28eaa16ec95a387fda6dc4fec
saif-uddin-shrabon <saif06065@gmail.com>
2023-02-26 12:45:42 +0600

    payment bug fixed

--- app/src/main/java/com/project/service_provider/admin/TransportDetailsInfo.java
+++ app/src/main/java/com/project/service_provider/admin/TransportDetailsInfo.java
@@ -1,5 +1,6 @@
 package com.project.service_provider.admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -17,18 +18,37 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.project.service_provider.MainActivity;
 import com.project.service_provider.R;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class TransportDetailsInfo extends AppCompatActivity {
 
-    LinearLayout firstL, scndL,thridL;
-    EditText fnumber,fammount,sotp;
-    Button paymentBKASH,Otp,done;
+    LinearLayout firstL, scndL,thridL,missingL;
+    private String verificationId;
+    private FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    EditText editTextCountryCode, editTextPhone,fammount,OTP,pinv;
+    Button buttonContinue,buttonVerify,EndButton,buttonPin;
     //dialog
     private static final int REQUEST_CALL = 1;
     TextView Name,vnumber,adress,drivincLicence,number,txtclose;
@@ -62,6 +82,9 @@ public class TransportDetailsInfo extends AppCompatActivity {
         System.out.println("Tdv: "+DrivingLicence);
         System.out.println("Tphone: "+phone);
 
+
+
+        //Payment Dialog
         payment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,6 +92,7 @@ public class TransportDetailsInfo extends AppCompatActivity {
                 myDialog.setContentView(R.layout.activity_paymentsystem);
                 txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
                 txtclose.setText("X");
+                mAuth = FirebaseAuth.getInstance();
                 txtclose.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -80,36 +104,81 @@ public class TransportDetailsInfo extends AppCompatActivity {
                 firstL =(LinearLayout) myDialog.findViewById(R.id.firstpase);
                 scndL = (LinearLayout) myDialog.findViewById(R.id.scndpase);
                 thridL =  (LinearLayout)myDialog.findViewById(R.id.thridpase);
+                missingL = (LinearLayout) myDialog.findViewById(R.id.missingStep);
 
-                fnumber = (EditText) myDialog.findViewById(R.id.bikashnumbr);
+                //first step part
                 fammount =  (EditText) myDialog.findViewById(R.id.ammount);
-                sotp =  (EditText) myDialog.findViewById(R.id.otp);
+                editTextCountryCode = (EditText) myDialog.findViewById(R.id.editTextCountryCode);
+                editTextPhone = (EditText) myDialog.findViewById(R.id.editTextPhone);
+                pinv = (EditText)myDialog.findViewById(R.id.editTextPin);
 
-                paymentBKASH = (Button) myDialog.findViewById(R.id.paymentB);
-                Otp = (Button) myDialog.findViewById(R.id.trsubmitBtn);
-                done =  (Button) myDialog.findViewById(R.id.done);
 
-                paymentBKASH.setOnClickListener(new View.OnClickListener() {
+                buttonContinue =(Button) myDialog.findViewById(R.id.buttonContinue);
+                buttonVerify =  (Button)myDialog.findViewById(R.id.buttonVerify);
+                buttonPin = (Button)myDialog.findViewById(R.id.buttonPin);
+                EndButton = (Button) myDialog.findViewById(R.id.done);
+                //otp part
+                progressBar = (ProgressBar) myDialog.findViewById(R.id.progressbar);
+                OTP =(EditText) myDialog.findViewById(R.id.editTextCode);
+
+                buttonContinue.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
+                        String code = editTextCountryCode.getText().toString().trim();
+                        String number = editTextPhone.getText().toString().trim();
+
+                        if (number.isEmpty() || number.length() < 10) {
+                            editTextPhone.setError("Valid number is required");
+                            editTextPhone.requestFocus();
+                            return;
+                        }
+
+                        String phoneNumber = code + number;
+                        sendVerificationCode(phoneNumber);
+
                         firstL.setVisibility(View.GONE);
                         scndL.setVisibility(View.VISIBLE);
                     }
                 });
 
-                Otp.setOnClickListener(new View.OnClickListener() {
+                buttonVerify.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        scndL.setVisibility(View.GONE);
-                        thridL.setVisibility(View.VISIBLE);
+
+                        String code = OTP.getText().toString().trim();
+
+                        if (code.isEmpty() || code.length() < 6) {
+
+                            OTP.setError("Enter code...");
+                            OTP.requestFocus();
+                            return;
+                        }
+                        verifyCode(code);
                     }
                 });
 
+                buttonPin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String pin = pinv.getText().toString();
 
-                done.setOnClickListener(new View.OnClickListener() {
+                        if(pin.isEmpty()){
+                            pinv.setError("Enter you pin");
+                            pinv.requestFocus();
+                        }else {
+                            missingL.setVisibility(View.GONE);
+                            thridL.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+                });
+
+                EndButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         myDialog.dismiss();
+                        thridL.setVisibility(View.VISIBLE);
                     }
                 });
 
@@ -179,4 +248,70 @@ public class TransportDetailsInfo extends AppCompatActivity {
             }
         }
     }
+
+    // FIREBASE PHONE AUTHENTICATION
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            scndL.setVisibility(View.GONE);
+                            missingL.setVisibility(View.VISIBLE);
+//                            Intent intent = new Intent(VerifyPhoneActivity.this, ProfileActivity.class);
+//                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//
+//                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(TransportDetailsInfo.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode(String number) {
+        progressBar.setVisibility(View.VISIBLE);
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number,
+                60L,
+                TimeUnit.SECONDS,
+                TransportDetailsInfo.this,
+                mCallBack
+        );
+
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null) {
+                OTP.setText(code);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(TransportDetailsInfo.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(View.GONE);
+        }
+    };
 }
